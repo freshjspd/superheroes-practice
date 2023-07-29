8fca181abf334b2ddbf433f64ef2f4ed67021acb
Viktoriia <freshjs.pd@gmail.com>
2023-07-29 12:33:56 +0300

    Fix server connection econnrefused

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 const httpServer = http.createServer(app);
 
-httpServer.listen(HOST, PORT, () =>
+httpServer.listen(PORT, HOST, () =>
   console.log(`Server is listening http://${HOST}:${PORT}`)
 );
