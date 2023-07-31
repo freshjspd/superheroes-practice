f515dd65356733e7f0d63e1a8c98bdac0b42ddd0
Viktoriia <freshjs.pd@gmail.com>
2023-07-31 23:21:16 +0300

    Add fileFilter

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,5 +1,25 @@
 const multer = require('multer');
 
-const upload = multer({ dest: 'public/images/' });
+const storage = multer.diskStorage({
+  // путь, куда сохранить файл
+  destination: (req, file, cb) => {
+    cb(null, 'public/images/');
+  },
+  // новое имя файла
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  cb(
+    null,
+    file.mimetype === 'image/jpeg' ||
+      file.mimetype === 'image/gif' ||
+      file.mimetype === 'image/png'
+  );
+};
+
+const upload = multer({ storage, fileFilter });
 
 module.exports.uploadHeroFile = upload.single('heroPhoto');
