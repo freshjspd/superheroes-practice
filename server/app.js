13d270355fd4242dfb23485f348712daff171998
Viktoriia <freshjs.pd@gmail.com>
2023-08-05 12:10:20 +0300

    Add cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const appRouter = require('./routes');
 const { errorHandlers } = require('./middleware');
 const { STATIC_FOLDER } = require('./constants');
 
 const app = express();
+
+app.use(cors({ origin: '*' }));
 
 app.use(express.static(STATIC_FOLDER));
 
