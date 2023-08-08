f4a78e5f83bc833f3074266bbed06a3cdbd4832b
Viktoriia <freshjs.pd@gmail.com>
2023-08-08 12:59:18 +0300

    Add hero form

--- client/src/components/HeroForm/index.jsx
+++ client/src/components/HeroForm/index.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 function HeroForm () {
-  return <div>HeroForm</div>
+  const initialValues = {
+    nickname: '',
+    realName: '',
+    originDescription: '',
+    catchPhrase: '',
+    isGood: true
+  }
+
+  const handleSubmit = (values, formikBag) => {}
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      {formikProps => (
+        <Form>
+          <label>
+            <span>Nickname:</span>
+            <Field type='text' name='nickname' placeholder='Nickname' />
+          </label>
+          <br />
+          <label>
+            <span>Real name:</span>
+            <Field type='text' name='realName' placeholder='Real Name' />
+          </label>
+          <br />
+          <label>
+            <span>Origin:</span>
+            <Field type='text' name='originDescription' placeholder='Origin' />
+          </label>
+          <br />
+          <label>
+            <span>Catch phrase:</span>
+            <Field type='text' name='catchPhrase' placeholder='Catch phrase' />
+          </label>
+          <br />
+          <label>
+            <Field type='checkbox' name='isGood' />
+            <span>Is hero positive</span>
+            <br />
+          </label>
+          <button type='submit'>Create</button>
+        </Form>
+      )}
+    </Formik>
+  )
 }
 
 export default HeroForm
